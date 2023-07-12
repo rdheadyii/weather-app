3b4f55e3a0723562b0644d92f628e21c87529e0d
rdheadyii <rdheadyii@gmail.com>
2023-07-12 09:08:43 -0400

    added pseudo code walkthrough of functions needed in script JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,7 +2,32 @@
 
 // api key
 
-// weather api main link
+// openweather api main link
 
-// weather history array
+// weather search history array
 
+
+// function to display search history
+    // loop over history array and count down so most recent at the top
+
+// function to update search history in local storage
+
+// function to get search history from local storage
+
+// function to display current weather from fetch request
+    // need icon, temp, humidity, date, wind speed, location name
+
+// function display 5 day forecast data from fetch request
+    // need icon, temp, humidity, date, wind speed
+
+
+// create elements to hold each of above
+
+// function to display 5 day for each of the cards
+
+// function for geolocation with lat lon
+    // call function above inside so data is usable
+
+// function for search button to work
+
+// function for history buttons to work
